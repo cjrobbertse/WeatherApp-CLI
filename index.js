976c256efc24d5d186b422f3b30b14cad3b08a1c
Christopher Robbertse <christopher.robbertse@vodafone.com>
2019-11-27 14:05:32 +0000

    contruct quotes

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const request = require('request');
 const argv = require('yargs').argv;
 
-let apiKey = '0b4d2c63acbd6b27807ea29bb53e48b4';
+const APIKEY = '8bad6775b898922bc78d3f42c358aa35';
 let city = argv.c || 'london';
-let url = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`;
 
 request(url, (err, response, body) => {
     if (err) {
         console.log('error:', error);
     } else {
-        console.log(body);
+        // console.log(body);
         let weather = JSON.parse(body);
         let message = `It's ${weather.main.temp} degrees in ${weather.name}`;
         console.log(message);
